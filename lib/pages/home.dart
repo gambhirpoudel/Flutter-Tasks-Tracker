1a4cca776814ff1951b28a6a30e806479bd9c324
Gambhir <gambhir.poudel@gmail.com>
2023-12-24 19:38:55 +0545

    Updated

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -41,85 +41,91 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: const CustomAppBar(title: 'Flutter Task Tracker'),
-      body: Column(
-        children: <Widget>[
-          for (var task in tasks)
-            Container(
-              height: 150,
-              width: 400,
-              margin: const EdgeInsets.all(8.0),
-              decoration: BoxDecoration(
-                color: const Color(0xFF64CCC5),
-                borderRadius: BorderRadius.circular(15.0),
-              ),
-              alignment: Alignment.center,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    'Title: ${task['title']}',
-                    style: const TextStyle(
-                      color: Color(0xFFDAFFFB),
-                      fontSize: 20,
-                      fontWeight: FontWeight.w500,
-                    ),
-                  ),
-                  Text(
-                    'Description: ${task['description']}',
-                    style: const TextStyle(
-                      color: Color(0xFFDAFFFB),
-                      fontSize: 16,
-                    ),
-                  ),
-                  Text(
-                    'Due Date: ${task['dueDate']}',
-                    style: const TextStyle(
-                      color: Color(0xFFDAFFFB),
-                      fontSize: 16,
-                    ),
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      ElevatedButton.icon(
-                        onPressed: () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) =>
-                                  EditTask(taskId: task['id']),
-                            ),
-                          ).then((result) {
-                            if (result == true) {
-                              _loadTasks();
-                            }
-                          });
-                        },
-                        style: ElevatedButton.styleFrom(
-                          backgroundColor: const Color(0xFFDAFFFB),
-                          foregroundColor: const Color(0xFF64CCC5),
-                        ),
-                        icon: const Icon(Icons.edit),
-                        label: const Text('Edit'),
+      body: ListView.builder(
+        itemCount: tasks.length,
+        itemBuilder: (context, int index) {
+          var task = tasks[index];
+          return Column(
+            children: <Widget>[
+              // for (var task in tasks)
+              Container(
+                height: 150,
+                width: 400,
+                margin: const EdgeInsets.all(8.0),
+                decoration: BoxDecoration(
+                  color: const Color(0xFF64CCC5),
+                  borderRadius: BorderRadius.circular(15.0),
+                ),
+                alignment: Alignment.center,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      'Title: ${task['title']}',
+                      style: const TextStyle(
+                        color: Color(0xFFDAFFFB),
+                        fontSize: 20,
+                        fontWeight: FontWeight.w500,
                       ),
-                      const SizedBox(width: 16),
-                      ElevatedButton.icon(
-                        onPressed: () {
-                          _delete(task['id']);
-                        },
-                        icon: const Icon(Icons.delete),
-                        label: const Text('Delete'),
-                        style: ElevatedButton.styleFrom(
-                          foregroundColor: const Color(0xFFDAFFFB),
-                          backgroundColor: Colors.red,
-                        ),
+                    ),
+                    Text(
+                      'Description: ${task['description']}',
+                      style: const TextStyle(
+                        color: Color(0xFFDAFFFB),
+                        fontSize: 16,
                       ),
-                    ],
-                  ),
-                ],
+                    ),
+                    Text(
+                      'Due Date: ${task['dueDate']}',
+                      style: const TextStyle(
+                        color: Color(0xFFDAFFFB),
+                        fontSize: 16,
+                      ),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        ElevatedButton.icon(
+                          onPressed: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) =>
+                                    EditTask(taskId: task['id']),
+                              ),
+                            ).then((result) {
+                              if (result == true) {
+                                _loadTasks();
+                              }
+                            });
+                          },
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: const Color(0xFFDAFFFB),
+                            foregroundColor: const Color(0xFF64CCC5),
+                          ),
+                          icon: const Icon(Icons.edit),
+                          label: const Text('Edit'),
+                        ),
+                        const SizedBox(width: 16),
+                        ElevatedButton.icon(
+                          onPressed: () {
+                            _delete(task['id']);
+                          },
+                          icon: const Icon(Icons.delete),
+                          label: const Text('Delete'),
+                          style: ElevatedButton.styleFrom(
+                            foregroundColor: const Color(0xFFDAFFFB),
+                            backgroundColor: Colors.red,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
               ),
-            ),
-        ],
+            ],
+          );
+        },
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
